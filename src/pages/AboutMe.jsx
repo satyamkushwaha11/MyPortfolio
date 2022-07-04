c97ed6ed8603467442e3c32b38d1cabea6ae112d
satyamkushwaha <satyamkushwaha.appdelite@gmail.com>
2022-07-04 08:17:38 +0530

    profile manu is complered in my profile

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -45,7 +45,7 @@ function AboutMe() {
   return (
     <div className='h-100  mx-4'>
 
-      <div className='position-absolute cancelLogoContainer'>
+      <div className={'position-absolute cancelLogoContainer'}>
         <i className={"close-button fas fa-times-circle fa-3x cancelLogo "} onClick={handelCancelPage} />
       </div>
       <div className='mx-3  mainPageHeader' >
@@ -60,9 +60,7 @@ function AboutMe() {
             Personal Info
           </span>
         </div>
-        {/* <div className='text-red'>
-          <i className="fas fa-horizontal-rule"></i>
-        </div> */}
+
         <div className={`bg-${color} `} style={{ width: "2rem", height: "5px", borderRadius: "5px" }} />
         <br />
         <div className='card bg-darkBule px-3 py-4'>
@@ -75,19 +73,20 @@ function AboutMe() {
           </div>
           <br />
           <div className='col-12 col-md-10'>
-            I am a MERN Stack Web Developer from Noida-UP,India. I am very passionate and dedicated to my work. I have 5 years more work experience. And enjoy working in a team or individual.          </div>
-          <div className='col-10 d-flex flex-md-row flex-wrap flex-column mt-3'>
+            I am a MERN Stack Web Developer from Noida-UP,India. I am very passionate and dedicated to my work. I have 5 years more work experience. And enjoy working in a team or individual.
+          </div>
+          <div className='col-12 d-flex flex-md-row flex-wrap flex-column mt-3'>
             {
               myDetails.map((item, index) => (
                 <>
-                  <div className='col-md-6 col-12 d-flex align-items-center my-2'>
+                  <div className='col-md-6 col-12 d-flex align-items-center my-md-2 my-0'>
                     <i className='fas fa-angle-double-right text-red'></i>
-                    <div className='ps-3'>
-                      <span className=''>
-                        <span className='me-2'>{item.key}</span>
-                        <span className=''>-</span>
-                        <span className='ms-2 text-dull'>{item.value}</span>
-                      </span>
+                    <div className='ps-3 '>
+
+                      <span className='pe-2'>{item.key}</span>
+                      <span className=''>-</span>
+                      <span className='ps-2 text-dull '>{item.value}</span>
+
                     </div>
                   </div>
                 </>
@@ -106,35 +105,22 @@ function AboutMe() {
       <div>
         <div className='mb-2'>
           <span style={{ fontSize: "1.2rem" }}>
-            Personal Info
+            Services
           </span>
         </div>
-
         <div className={`bg-${color} `} style={{ width: "2rem", height: "5px", borderRadius: "5px" }} />
         <br />
         <div className='card bg-darkBule px-3 py-4'>
-          <div className='w-100 h-100 d-md-none ' style={{ border: `10px solid ${color}`, borderRadius: "8px" }}>
+          <div className='col-12 d-flex flex-md-row flex-column mt-3'>
+            <div className='  h-25 col-md-6 col-12'>
+              coming soon
+            </div>
 
-            <MyImage style={{
-              outline: `4px solid ${color}`,
-              borderRadius: "8px"
-            }} />
-          </div>
-          <br />
-          <div className='col-12 col-md-10'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit pariatur saepe architecto, tempora placeat est illo perspiciatis culpa nihil. Repudiandae suscipit quis atque maiores ex dolor hic illum nemo voluptates!
-          </div>
-          <div className='col-10 d-flex flex-md-row flex-column mt-3'>
-            <div className='col-md-6 col-12'>
-              sssssss
-            </div>
-            <div className='col-md-6 col-12'>
-              lllllll
-            </div>
 
           </div>
         </div>
       </div>
+      <br />
     </div>
   )
 }
